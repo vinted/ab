c5bf9e606a87834661f11ebe43c3b07b47ca6d7b
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2014-05-23 17:04:15 +0300

    Fix hooks - pass params to the lowest level

--- lib/ab/experiments.rb
+++ lib/ab/experiments.rb
@@ -3,11 +3,15 @@ module Ab
     include Hooks
     define_hooks :before_picking_variant, :after_picking_variant
 
+    Ab::AssignedExperiment.before_picking_variant do |experiment|
+      Ab::Experiments.run_hook :before_picking_variant, experiment
+    end
+    Ab::AssignedExperiment.after_picking_variant do |experiment, variant|
+      Ab::Experiments.run_hook :after_picking_variant, experiment, variant
+    end
+
     def initialize(config, id)
       @assigned_experiments ||= {}
-
-      AssignedExperiment.before_picking_variant { run_hook :before_picking_variant }
-      AssignedExperiment.after_picking_variant { run_hook :after_picking_variant }
 
       (config['ab_tests'] || []).each do |experiment|
         name = experiment['name']
